5393aaa5db4715f7dc97de1eb4489539d4edbb25
JUNGHWAN-LEE <flowerjsn@gmail.com>
2026-07-12 02:04:46 +0900

    auto: lcc 작업 완료 2026-07-12 02:04

--- Users/jason/.next-builds/lcc/dev/server/middleware-build-manifest.js
+++ Users/jason/.next-builds/lcc/dev/server/middleware-build-manifest.js
@@ -1,13 +1,13 @@
 globalThis.__BUILD_MANIFEST = {
-  "pages": {
-    "/_app": []
+  pages: {
+    "/_app": [],
   },
-  "devFiles": [],
-  "polyfillFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": []
+  devFiles: [],
+  polyfillFiles: [],
+  lowPriorityFiles: [],
+  rootMainFiles: [],
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+  "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+  "/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
 ];
